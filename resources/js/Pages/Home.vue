6c5b7d2cac641a838cbd1eaa07afcb38a6702b8a
Thinus <thinus.barnard@payfast.co.za>
2021-08-04 16:02:15 +0200

    WIP

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,7 +6,10 @@
 
 <script>
 export default {
-    name: "Home"
+    name: "Home",
+    async mounted() {
+        console.log("mounted")
+    }
 }
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\ScraperController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/scrape', [ScraperController::class, 'index'])->name('scrape');
 Route::get('/{any?}', [AppController::class, 'index']);
